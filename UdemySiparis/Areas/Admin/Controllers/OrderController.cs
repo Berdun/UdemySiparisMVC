cb9ed5c6786058455420cfe614a0e2dc6ca52254
Bahadır ERDUN <bahadirerdun@windowslive.com>
2022-02-20 13:09:51 +0300

    0101

--- UdemySiparis/Areas/Admin/Controllers/OrderController.cs
+++ UdemySiparis/Areas/Admin/Controllers/OrderController.cs
@@ -22,15 +22,59 @@ namespace UdemySiparis.Areas.Admin.Controllers
         }
 
 
-        public IActionResult Details(int orderId)
+        public IActionResult Details(int Id)
         {
             OrderVM = new OrderVM
             {
-                OrderProduct = _unitOfWork.OrderProduct.GetFirstOrDefault(o => o.Id == orderId, includeProperties: "AppUser"),
-                OrderDetails = _unitOfWork.OrderDetails.GetAll(d => d.OrderProductId == orderId, includeProperties: "Product")
+                OrderProduct = _unitOfWork.OrderProduct.GetFirstOrDefault(o => o.Id == Id, includeProperties: "AppUser"),
+                OrderDetails = _unitOfWork.OrderDetails.GetAll(d => d.OrderProductId == Id, includeProperties: "Product")
             };
             return View(OrderVM);
         }
+
+        [HttpPost]
+        public IActionResult Delivered(OrderVM orderVM)
+        {
+            var orderProduct = _unitOfWork.OrderProduct.GetFirstOrDefault(o => o.Id == orderVM.OrderProduct.Id);
+
+            orderProduct.OrderStatus = "Delivered";
+
+            _unitOfWork.OrderProduct.Update(orderProduct);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Details","Order", new {Id = orderVM.OrderProduct.Id});
+        }
+
+        [HttpPost]
+        public IActionResult CancelOrder(OrderVM orderVM)
+        {
+            var orderProduct = _unitOfWork.OrderProduct.GetFirstOrDefault(o => o.Id == orderVM.OrderProduct.Id);
+
+            orderProduct.OrderStatus = "Cancel";
+
+            _unitOfWork.OrderProduct.Update(orderProduct);
+            _unitOfWork.Save();
+
+
+            return RedirectToAction("Details", "Order", new { Id = orderVM.OrderProduct.Id });
+        }
+
+        [HttpPost]
+        public IActionResult UpdateOrderDetails(OrderVM orderVM)
+        {
+            var orderProduct = _unitOfWork.OrderProduct.GetFirstOrDefault(o => o.Id == orderVM.OrderProduct.Id);
+
+            orderProduct.Address = orderVM.OrderProduct.Address;
+            orderProduct.PostalCode = orderVM.OrderProduct.PostalCode;
+            orderProduct.CellPhone = orderVM.OrderProduct.CellPhone;
+            orderProduct.Name = orderVM.OrderProduct.Name;
+
+            _unitOfWork.OrderProduct.Update(orderProduct);
+            _unitOfWork.Save();
+
+            return RedirectToAction("Details", "Order", new { Id = orderVM.OrderProduct.Id });
+        }
+
 
 
     }
